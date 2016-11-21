181d1f0a012d2a723172b654497dc4725170af4c
Senaps <gerdakan.sa@gmail.com>
2016-11-21 22:27:34 +0330

    last fix on structure!

--- twitter.py
+++ twitter.py
@@ -1,24 +1,31 @@
-from flask import Flask, render_template, request, url_for, session
+from flask import Flask, render_template, request, url_for, session, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'you will never guess it!'
 
-@app.route('/', methods=['GET'])
+@app.route('/index/')
+@app.route('/')
 def index():
     if 'user' in session:
-        return url_for('home')
+        return redirect(url_for('home'))
     return render_template('index.html')
 
-@app.route('/home', methods=['GET'])
+@app.route('/home')
 def home():
     if 'user' in session:
-        pass #show the twit's...
+        return 'you are here!'
     else:
-        pass #show the texts....
+        return redirect(url_for('index'))
 
 @app.route('/login', methods=['POST'])
 def login():
-    pass
+    session['user'] = 'username'
+    return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('index'))
         
 if __name__ == '__main__':
     app.run(debug=True)
